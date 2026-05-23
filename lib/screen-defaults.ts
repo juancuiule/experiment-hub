a380839c583efc67f1b6058216ce07de3ef82b46
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2026-05-23 00:38:45 -0300

    add comments to default from component

--- lib/screen-defaults.ts
+++ lib/screen-defaults.ts
@@ -39,6 +39,12 @@ function defaultFromComponent(
     case 'control': {
       switch (component.template) {
         case 'conditional': {
+          // We can't determine which branch of the condition will be
+          // taken at build time, so we evaluate the condition with the
+          // current context and return defaults for the branch that is
+          // taken. This means that if the condition depends on user
+          // input or other dynamic data, the defaults may not be accurate
+          // until that data is available in the context.
           const condition = resolveCondition(component.props.if, context);
           const branch = evaluateCondition(condition, context)
             ? component.props.component
@@ -46,6 +52,10 @@ function defaultFromComponent(
           return branch ? defaultFromComponent(branch, context) : {};
         }
         case 'for-each': {
+          // If iter values are static, we can compute defaults at build time.
+          // If they are dynamic we try to get the current value from context,
+          // but if it's not there we return an empty array since we don't
+          // know how many iterations there will be and which values they will have.
           const iterValues =
             component.props.type === 'static'
               ? component.props.values
@@ -54,6 +64,10 @@ function defaultFromComponent(
                   | null) ?? []);
           const inner = component.props.component;
 
+          // Remap the inner component for each iter value,
+          // resolving any data keys that depend on the iter value.
+          // TODO: check if this happens multiple times and check if we should
+          // TODO: resolve other values too, like labels that might depend on iter values.
           const components = iterValues.map((value, i) => {
             const newContext = mergeContext(context, {
               screenData: {
